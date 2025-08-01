300f70003ef9b3108f63bf7a1a37141b83441d0e
Yadnyesh Patil <38451953+yadn@users.noreply.github.com>
2025-08-01 22:12:12 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 
     // Forward to Google Apps Script (or anywhere)
     try {
-      await fetch('https://script.google.com/macros/s/YOUR_ID/exec', {
+      await fetch('https://script.google.com/macros/s/AKfycbwa5Yg6Eryx4Avmp98o711aSuUtgB4JOAZVW82RtPeCdqKcBEVFTs7X1NsNTnkcONJoVw/exec', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(req.body),
